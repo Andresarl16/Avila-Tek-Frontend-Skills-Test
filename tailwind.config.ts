d8427e861e48794ab01233ba07fcb09c3900624a
AndresARL <andres.rojas5722@gmail.com>
2024-02-16 19:47:04 -0400

    chore(Configuration): Create Color Variables in Tailwind Configuration File

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,33 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        // Background Colors //
+        "bg-primary": "#FFFFFF",
+        "bg-secondary": "#F9FAFB",
+        "bg-brand-section": "#53389E",
+        "bg-button-primary": "#7F56D9",
+        "bg-button-secondary": "#FFFFFF",
+
+        // Text Colors //
+        "text-primary": "#101828",
+        "text-tertiary": "#475467",
+        "text-brand-primary": "#FFFFFF",
+        "text-brand-secondary": "#E9D7FE",
+        "text-brand-tertiary": "#E9D7FE",
+        "text-placeholder": "#667085",
+        "text-button-primary": "#FFFFFF",
+        "text-button-tertiary": "#6941C6",
+
+        // Border Colors //
+        "border-primary": "#D0D5DD",
+        "border-button-primary": "#7F56D9",
+        "border-button-secondary": "#D0D5DD",
+
+        // Utility //
+        "utility-brand-50": "#F9F5FF",
+        "utility-brand-200": "#E9D7FE",
+        "utility-brand-700": "#6941C6",
       },
     },
   },
