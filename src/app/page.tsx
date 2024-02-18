f9d2afdaa4cc9af5fedbb0a4da931293ad0fced6
AndresARL <andres.rojas5722@gmail.com>
2024-02-18 10:38:09 -0400

    feat(Sections): Implement Free Trial Section
    Missing responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,6 +124,73 @@ export default function Home() {
           iconSizes="small"
         />
       </section>
+      <section className="grid grid-cols-12 bg-bg-secondary py-24">
+        <div className="grid col-start-2 col-span-10 grid-cols-12">
+          <div className="flex flex-col col-span-7 justify-center">
+            <h2 className="text-5xl text-text-primary font-semibold">
+              No long-term contracts. <br /> No catches.
+            </h2>
+            <p className="text-xl mt-6 mb-12 text-text-tertiary">
+              Start your 30-day free trial today.
+            </p>
+            <div className="sm:flex space-x-0 space-y-3 sm:space-x-3 sm:space-y-0">
+              <Button
+                text="Learn more"
+                type="secondary"
+                redirectTo=""
+                styles="border-2 border-border-button-secondary"
+              />
+              <Button text="Get started" type="primary" redirectTo="" />
+            </div>
+          </div>
+          <div className="col-span-5 space-y-5">
+            <div className="flex justify-center items-end space-x-5">
+              <div className="relative w-40 h-40">
+                {" "}
+                <Image
+                  alt=""
+                  src={"/static/free-trial-mobile-image-1.png"}
+                  fill={true}
+                />{" "}
+              </div>
+              <div className="relative w-40 h-60">
+                {" "}
+                <Image
+                  alt=""
+                  src={"/static/free-trial-mobile-image-2.png"}
+                  fill={true}
+                />{" "}
+              </div>
+            </div>
+            <div className="flex justify-center items-start space-x-5">
+              <div className="relative w-48 h-32">
+                {" "}
+                <Image
+                  alt=""
+                  src={"/static/free-trial-mobile-image-5.png"}
+                  fill={true}
+                />{" "}
+              </div>
+              <div className="relative w-40 h-60">
+                {" "}
+                <Image
+                  alt=""
+                  src={"/static/free-trial-mobile-image-3.png"}
+                  fill={true}
+                />{" "}
+              </div>
+              <div className="relative w-48 h-32">
+                {" "}
+                <Image
+                  alt=""
+                  src={"/static/free-trial-mobile-image-4.png"}
+                  fill={true}
+                />{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
