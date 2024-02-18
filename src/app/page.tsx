40b350453ccd131830eb3a6b1e88f4a20f57e352
AndresARL <andres.rojas5722@gmail.com>
2024-02-17 22:49:59 -0400

    fix(Responsive): Fix Header Responsive Details
    Change margins and padding responsive, add responsive to charts, add image sizes to Next Image Components, add priority to image loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,14 +16,14 @@ export default function Home() {
 
   return (
     <main className="max-w-full">
-      <header className="grid grid-cols-12 mt-8 mb-32">
+      <header className="grid grid-cols-12 mt-0 sm:mt-8 mb-24 sm:mb-28 lg:mb-32">
         <div className="grid sm:col-start-2 col-span-12 sm:col-span-10 grid-cols-12 px-4 md:px-0 bg-bg-brand-section sm:rounded-2xl">
           <div className="sm:col-start-3 col-span-12 sm:col-span-8 pt-16 sm:pt-24 pb-24 sm:pb-48">
-            <h1 className="text-4xl sm:text-7xl text-text-brand-primary text-center font-semibold">
+            <h1 className="text-4xl md:text-5xl lg:text-7xl text-text-brand-primary text-center font-semibold">
               Grow your users. <br />
               <span className="text-text-brand-secondary">Smarter.</span>
             </h1>
-            <p className="mt-4 sm:mt-6 mb-10 sm:mb-12 font-lg sm:font-xl text-text-brand-tertiary text-center">
+            <p className="mt-4 sm:mt-5 lg:mt-6 mb-10 sm:mb-11 lg:mb-12 font-lg sm:font-xl text-text-brand-tertiary text-center">
               Powerful, self-serve product and growth analytics to help you
               convert, engage, and retain more users. Trusted by over 4,000
               startups.
@@ -45,23 +45,26 @@ export default function Home() {
             </form>
           </div>
         </div>
-        <div className="relative col-start-2 sm:col-start-4 col-span-10 sm:col-span-6 -mt-8 sm:-mt-24 p-8 bg-bg-primary rounded-xl border-2 border-border-secondary drop-shadow-xl">
+        <div className="relative col-start-2 sm:col-start-3 xl:col-start-4 col-span-10 sm:col-span-8 xl:col-span-6 -mt-8 sm:-mt-24 p-8 bg-bg-primary rounded-xl border-2 border-border-secondary drop-shadow-xl">
           <h5 className="text-lg text-text-primary font-semibold">
             Users over time
           </h5>
-          <div className="relative h-52 sm:h-96">
+          <div className="relative h-52 sm:h-60 md:h-72 xl:h-96">
             <Image
               alt="Graphic of users over time"
               src={"/static/line-and-bar-chart.jpg"}
               fill={true}
+              priority={true}
+              sizes="84vw, (min-width: 640px) 68vw, (min-width: 1280px) 50vw"
             ></Image>
           </div>
 
-          <div className="absolute w-48 sm:w-60 h-48 sm:h-60 -right-20 -bottom-9">
+          <div className="absolute w-48 sm:w-52 md:w-56 lg:w-60 h-48 sm:h-52 md:h-56 lg:h-60 -right-20 -bottom-9">
             <Image
               alt="Graphic of users over time"
               src={"/static/activity-gauge.png"}
               fill={true}
+              sizes="192px, (min-width: 640px) 208px, (min-width: 768px) 224px, (min-width: 1024px) 240px"
             ></Image>
           </div>
         </div>
@@ -106,6 +109,7 @@ export default function Home() {
               alt="Give us a shoot image"
               src={"/static/give-us-a-shoot.jpg"}
               fill={true}
+              sizes="85vw, (min-width: 1024px) 35vw"
               className="object-cover rounded-b-2xl lg:rounded-r-2xl"
             />
           </div>
