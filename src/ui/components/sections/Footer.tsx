9c224427b56efd33a11111e5573d935427cd41bc
AndresARL <andres.rojas5722@gmail.com>
2024-02-19 17:25:48 -0400

    feat(Responsive): Footer Responsive

--- src/ui/components/sections/Footer.tsx
+++ src/ui/components/sections/Footer.tsx
@@ -7,8 +7,8 @@ import LogoIcon from "@/ui/icons/LogoIcon";
 
 export default function Footer() {
   return (
-    <footer className="grid sm:col-start-2 col-span-12 sm:col-span-10 grid-cols-12 mt-16 mb-12">
-      <div className="flex flex-row justify-between col-span-12">
+    <footer className="grid sm:col-start-2 col-span-12 sm:col-span-10 grid-cols-12 mt-12 sm:mt-16 mb-12 px-4 sm:px-0">
+      <div className="flex flex-col-reverse sm:flex-row justify-between col-span-12">
         <ul className="flex space-x-8">
           {footer.linkColumns.map((column) => (
             <li key={column.name}>
@@ -32,24 +32,26 @@ export default function Footer() {
             </li>
           ))}
         </ul>
-        <ul className="space-y-4">
+        <div className="space-y-4 mb-12 sm:mb-0">
           <p className="text-sm text-text-primary font-semibold">
             {" "}
             {footer.downloadBadges.name}{" "}
           </p>
-          {footer.downloadBadges.badges.map((badge) => (
-            <li key={badge.redirectTo} className="cursor-pointer">
-              <a href={badge.redirectTo} target="_blank">
-                <badge.svg width={135} height={40} />
-              </a>
-            </li>
-          ))}
-        </ul>
+          <ul className="flex flex-row sm:flex-col space-x-4 sm:space-x-0 space-y-0 sm:space-y-4">
+            {footer.downloadBadges.badges.map((badge) => (
+              <li key={badge.redirectTo} className="cursor-pointer">
+                <a href={badge.redirectTo} target="_blank">
+                  <badge.svg width={135} height={40} />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <hr className="col-span-12 mt-16 mb-8 border-border-secondary" />
-      <div className="flex justify-between col-span-12">
+      <hr className="col-span-12 mt-12 sm:mt-16 mb-8 border-border-secondary" />
+      <div className="flex flex-col sm:flex-row justify-between col-span-12">
         <LogoIcon width={145} height={38}></LogoIcon>
-        <p className="text-text-quaternary">
+        <p className="text-text-quaternary mt-6 sm:mt-0">
           © 2077 Untitled UI. All rights reserved.
         </p>
       </div>
