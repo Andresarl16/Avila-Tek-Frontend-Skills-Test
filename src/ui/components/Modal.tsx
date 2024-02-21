b805335db6e4a4205cbee837de7e51b8f45fb490
AndresARL <andres.rojas5722@gmail.com>
2024-02-21 18:23:32 -0400

    feat(Responsive): Add responsive to Modal

--- src/ui/components/Modal.tsx
+++ src/ui/components/Modal.tsx
@@ -25,23 +25,25 @@ export default function Modal({
   };
 
   return (
-    <div
-      onClick={handleBackdropClick}
-      className="fixed flex min-h-[544px] justify-center items-center z-20 inset-0 bg-black bg-opacity-25 backdrop-blur-sm"
-    >
-      <div className="w-full max-w-md m-4 p-4 text-text-primary bg-bg-primary rounded-2xl">
-        <header className="flex justify-between items-center px-4">
-          <h3 className="text-3xl text-center flex-grow ml-5"> {title} </h3>
-          <CloseIcon
-            onClick={() => onClose()}
-            className="cursor-pointer ml-auto"
-            width={20}
-            height={20}
-          />
-        </header>
-        <hr className="my-2 border-border-secondary" />
-        <div className="max-h-96 overflow-auto mx-2 my-6">{children}</div>
+    <main className="fixed overflow-scroll h-screen flex justify-center z-20 inset-0 bg-black bg-opacity-25 backdrop-blur-sm">
+      <div className="my-auto">
+        <div className="w-[calc(100%-32px)] max-w-md m-4 p-4 h-[540px] text-text-primary bg-bg-primary rounded-2xl">
+          <header className="flex justify-between items-center px-4">
+            <h3 className="text-xl sm:text-3xl text-center flex-grow">
+              {" "}
+              {title}{" "}
+            </h3>
+            <CloseIcon
+              onClick={() => onClose()}
+              className="cursor-pointer ml-1"
+              width={15}
+              height={15}
+            />
+          </header>
+          <hr className="my-2 border-border-secondary" />
+          <div className="max-h-96 overflow-auto mx-2 my-6">{children}</div>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
